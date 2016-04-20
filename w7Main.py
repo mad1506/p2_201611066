81445e627796713c6a194466b3224c38a2927aec
mad1506 <brave_devil@naver.com>
2016-04-20 13:21:10 +0900

    Update w7Main.py

--- w7Main.py
+++ w7Main.py
@@ -26,6 +26,13 @@ def drawSquareFrom(tracks):
     tracks.append((200,200))
     for i in range(0,4):
         drawSquareAt(100,tracks[i])
+        
+def replayTracks(myTracks):
+    t1.clear()
+    t1.color("blue")
+    t1.speed(5)
+    for t in myTracks:
+        t1.goto(t)
 
 def lab7():
     global wn
@@ -36,6 +43,50 @@ def lab7():
     mytrack=drawSquareAt2(100,(10,10))
     print mytrack
     drawSquareFrom(100)
+    
+    import turtle
+    wn=turtle.Screen()
+    #wn.bgpic("myMaze.gif")
+    t1=turtle.Turtle()
+    t1.speed(6)
+    t1.penup()
+
+    myTracks=list()
+
+    t1.goto(-400,300)
+    myTracks.append(t1.pos())
+    t1.pendown()
+    t1.pencolor("Red")
+    t1.right(90)
+    t1.fd(400)
+    myTracks.append(t1.pos())
+    t1.backward(150)
+    myTracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(300)
+    myTracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(300)
+    myTracks.append(t1.pos())
+    t1.back(150)
+    myTracks.append(t1.pos())
+    t1.right(90)
+    t1.fd(300)
+    myTracks.append(t1.pos())
+    t1.left(90)
+    t1.right(90)
+    t1.right(90)
+    t1.fd(200)
+    myTracks.append(t1.pos())
+    t1.fd(300)
+    myTracks.append(t1.pos())
+    t1.back(100)
+    myTracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(200)
+    myTracks.append(t1.pos())
+    
+    replayTracks(myTracks)
     
 def main():
     lab7()
